feb1c1a9a577faf6bc4f36d6b8d454d94468b01c
SobhanRjz <sobhan.rajabzadeh@gmail.com>
2026-06-30 12:36:38 +0330

    feat: add database gym table

---
+++ backend/app/Models/Gym.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Gym extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function members(): HasMany
+    {
+        return $this->hasMany(Member::class);
+    }
+
+    public function fitnessClasses(): HasMany
+    {
+        return $this->hasMany(FitnessClass::class);
+    }
+}
